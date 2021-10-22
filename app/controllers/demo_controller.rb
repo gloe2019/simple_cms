1e78d4a53cfdc6c1aa368705b07e997776276084
gloe2019 <gloedem@gmail.com>
2021-10-22 04:27:27 -0400

    add about and contact actions to demo controller + templates

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,32 @@
 class DemoController < ApplicationController
-  layout false #turn off behavior to use layout with views...
+  layout false # turn off behavior to use layout with views...
+  # Instance variables allow the controller provide data to the view templates..
   def index
+  end
+
+  def hello
+    @array = [1, 2, 3, 4, 5]
+    @id = params['id']
+    @page = params[:page]
+    # render('index') -- render ERB template
+    # redirect_to{:controller => 'demo', :action => 'index'}
+  end
+
+  def about
+    render('about_us')
+  end
+  
+  def contact
+    puts params
+    @country = params[:country]
+    if ['us', 'ca'].include?(@country)
+      @phone = '(800) 555-6789'
+    elsif @country == 'uk'
+      @phone = "(020) 7946 1234"
+    else
+      @phone = "+1 (987) 654-3210"
+    end
+    puts @phone
+    render('contact_us')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/hello'
+
+  get 'demo/about'
+  get 'demo/contact'
+  
+  # default route
+  # default route
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
